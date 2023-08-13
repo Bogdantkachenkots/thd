408e65ac4859e1a83158150a70df045de7bb83a0
Walter Huf <hufman@gmail.com>
2023-08-12 21:37:54 -0700

    Upgrades Sentry SDK

--- app/src/sentry/java/me/hufman/androidautoidrive/Analytics.kt
+++ app/src/sentry/java/me/hufman/androidautoidrive/Analytics.kt
@@ -2,67 +2,72 @@ package me.hufman.androidautoidrive
 
 import android.content.Context
 import io.sentry.Sentry
-import io.sentry.android.AndroidSentryClientFactory
-import io.sentry.event.Event
-import io.sentry.event.EventBuilder
-import io.sentry.event.interfaces.ExceptionInterface
+import io.sentry.SentryEvent
+import io.sentry.SentryLevel
+import io.sentry.android.core.SentryAndroid
+import io.sentry.protocol.Message
 import me.hufman.androidautoidrive.music.MusicAppInfo
 
 object Analytics: AnalyticsProvider {
 	override fun init(context: Context) {
-		Sentry.init(AndroidSentryClientFactory(context))
-	}
-
-	override fun reportMusicAppProbe(appInfo: MusicAppInfo) {
-		val event = EventBuilder()
-				.withMessage("Probed music app")
-				.withTag("appId", appInfo.packageName)
-				.withTag("appName", appInfo.name)
-				.withTag("controllable", if (appInfo.controllable) "true" else "false")
-				.withTag("connectable", if (appInfo.connectable) "true" else "false")
-				.withTag("browseable", if (appInfo.browseable) "true" else "false")
-				.withTag("searchable", if (appInfo.searchable) "true" else "false")
-				.withTag("playsearchable", if (appInfo.playsearchable) "true" else "false")
-				.withLevel(Event.Level.DEBUG)
-		Sentry.capture(event)
-	}
-
-	override fun reportCarProbeFailure(port: Int, message: String?, throwable: Throwable?) {
-		val event = EventBuilder()
-				.withMessage("Failed to probe car: $message")
-				.withTag("port", port.toString())
-				.withLevel(Event.Level.WARNING)
-		if (throwable != null) {
-			val crash = event
-					.withSentryInterface(ExceptionInterface(throwable))
-			Sentry.capture(crash)
-		} else {
-			Sentry.capture(event)
+		SentryAndroid.init(context) { options ->
+			options.dsn = context.applicationInfo.metaData.getString("io.sentry.dsn")
+			options.isEnableAutoSessionTracking = false
+			options.enableAllAutoBreadcrumbs(false)
+			options.isAttachServerName = false
+			options.isAttachStacktrace = false
 		}
 	}
 
+	override fun reportMusicAppProbe(appInfo: MusicAppInfo) {
+		val event = SentryEvent().apply {
+			message = Message().also { it.message = "Probed music app"}
+			setTag("appId", appInfo.packageName)
+			setTag("appName", appInfo.name)
+			setTag("controllable", if (appInfo.controllable) "true" else "false")
+			setTag("connectable", if (appInfo.connectable) "true" else "false")
+			setTag("browseable", if (appInfo.browseable) "true" else "false")
+			setTag("searchable", if (appInfo.searchable) "true" else "false")
+			setTag("playsearchable", if (appInfo.playsearchable) "true" else "false")
+			level = SentryLevel.DEBUG
+		}
+		Sentry.captureEvent(event)
+	}
+
+	override fun reportCarProbeFailure(port: Int, message: String?, throwable: Throwable?) {
+		val event = SentryEvent().apply {
+			this.message = Message().also { it.message = "Failed to probe car: $message" }
+			setTag("port", port.toString())
+			level = SentryLevel.WARNING
+			throwable?.let { setThrowable(it) }
+		}
+		Sentry.captureEvent(event)
+	}
+
 	override fun reportCarProbeDiscovered(port: Int?, vehicleType: String?, hmiType: String?) {
-		val event = EventBuilder()
-				.withMessage("Successfully probed to connect to car")
-				.withTag("vehicleType", vehicleType ?: "")
-				.withTag("hmiType", hmiType ?: "")
-				.withTag("port", port.toString())
-				.withLevel(Event.Level.DEBUG)
-		Sentry.capture(event)
+		val event = SentryEvent().apply {
+			message = Message().also { it.message = "Successfully probed to connect to car" }
+			setTag("vehicleType", vehicleType ?: "")
+			setTag("hmiType", hmiType ?: "")
+			setTag("port", port.toString())
+			level = SentryLevel.DEBUG
+		}
+		Sentry.captureEvent(event)
 	}
 
 	override fun reportCarCapabilities(capabilities: Map<String, String?>) {
-		var event = EventBuilder()
-				.withMessage("Car capabilities")
-				.withLevel(Event.Level.DEBUG)
+		val event = SentryEvent().apply {
+			message = Message().also { it.message = "Car capabilities" }
+			level = SentryLevel.DEBUG
+		}
 		capabilities.keys.forEach { key ->
 			val value = capabilities[key]
 			if (value != null) {
 				val keyName = key.replace(Regex("[^a-zA-Z0-9_]"), "_")
-				event.withTag(keyName, value.toString())
+				event.setTag(keyName, value.toString())
 			}
 		}
-		Sentry.capture(event)
+		Sentry.captureEvent(event)
 	}
 
 }
